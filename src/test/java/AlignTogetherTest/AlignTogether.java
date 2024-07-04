01940bbbc8172e4dfa3fd9fa25d643997aaf2381
abhinav <abhinavdwivedi0807@gmail.com>
2024-07-04 08:26:04 +0530

    initial_Login

---
+++ src/test/java/AlignTogetherTest/AlignTogether.java
@@ -0,0 +1,61 @@
+package AlignTogetherTest;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class AlignTogether {
+
+	String url ="https://aligntogether.live/";
+	WebDriver driver;
+
+	public void invokeBrowser() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\abhin\\eclipse-workspace\\libs\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+		driver.get(url);
+	}
+
+	public void sessionBookingPage() {
+		driver.findElement(By.xpath("//div/a[text()='Book Your Service']")).click();
+	}
+
+	public void booking() {	
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		WebElement card = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[1]/div/div/main/div/div[2]/div[1]/div/div/div/div/div/div[3]/a/div/div/div[3]/div/a")));
+		card.click();
+
+	}
+
+	public void loginPageRedirect() {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		WebElement login = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@class=\"ant-btn ant-btn-primary\"]/span[text()='Contact Seller']")));
+		login.click();
+	}
+	  public void login() {
+		  WebDriverWait  wait = new WebDriverWait(driver, Duration.ofSeconds(6));
+		  WebElement flagsetting = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='selected-flag']")));
+		  flagsetting.click();
+
+	}
+	public void closeBrowser() {
+		driver.navigate().refresh();
+		driver.close();
+	}
+}
+
+
+// Trying to login but was unable to git in(unknown error kinds unable to fetch the xpath due  to dynamic nature i guess)
+//public void loginUser(){
+//	driver.navigate().refresh();
+//    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+//    WebElement loginButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div/a[text()=\"Book Your Service\"]")));
+//    loginButton.click();
+//}
---
+++ src/test/java/AlignTogetherTest/AlignTogetherMainClass.java
@@ -0,0 +1,15 @@
+package AlignTogetherTest;
+
+public class AlignTogetherMainClass {
+
+	public static void main(String[] args){
+		AlignTogether ag =new AlignTogether();
+		ag.invokeBrowser();
+		ag.sessionBookingPage();
+		ag.booking();
+		ag.loginPageRedirect();
+		ag.login();
+		
+	}
+
+}
