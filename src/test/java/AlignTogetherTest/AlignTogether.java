7c5f839859c1fbd8082296e808c6bb01807d67fd
abhinav <abhinavdwivedi0807@gmail.com>
2024-07-05 07:50:16 +0530

    login-reached

--- src/test/java/AlignTogetherTest/AlignTogether.java
+++ src/test/java/AlignTogetherTest/AlignTogether.java
@@ -8,45 +8,55 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.*;
 
 public class AlignTogether {
 
 	String url ="https://aligntogether.live/";
 	WebDriver driver;
 
+	@Test
 	public void invokeBrowser() {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\abhin\\eclipse-workspace\\libs\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
-		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
 		driver.get(url);
 	}
-
+	@Test(dependsOnMethods = "invokeBrowser")
 	public void sessionBookingPage() {
 		driver.findElement(By.xpath("//div/a[text()='Book Your Service']")).click();
 	}
-
+	
+	@Test(dependsOnMethods = "sessionBookingPage")
 	public void booking() {	
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 		WebElement card = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[1]/div/div/main/div/div[2]/div[1]/div/div/div/div/div/div[3]/a/div/div/div[3]/div/a")));
-		card.click();
-
-	}
-
+		card.click();}
+	
+	
+	@Test(dependsOnMethods = "booking")
 	public void loginPageRedirect() {
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 		WebElement login = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@class=\"ant-btn ant-btn-primary\"]/span[text()='Contact Seller']")));
-		login.click();
+		login.click();}
+	
+	
+	@Test(dependsOnMethods = "loginPageRedirect")
+	public void login() {
+		WebDriverWait  wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		WebElement flagsetting = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[1]/div/div/div/div[1]/div/div/div/div[1]/input")));
+		flagsetting.click();
+
+		WebElement indianFlag = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='India (भारत)']")));
+		indianFlag.click();
+	
+	driver.findElement(By.xpath("//div[@class='flag-container']")).sendKeys("hichka@mailinator.com");
+	driver.findElement(By.xpath("//input[@name='password']")).sendKeys("Hichka");
 	}
-	  public void login() {
-		  WebDriverWait  wait = new WebDriverWait(driver, Duration.ofSeconds(6));
-		  WebElement flagsetting = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='selected-flag']")));
-		  flagsetting.click();
-		 
-		  WebElement indianFlag = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='India (भारत)']")));
-		  indianFlag.click();
-	}
+	
+
 	public void closeBrowser() {
 		driver.navigate().refresh();
 		driver.close();
